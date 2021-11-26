a36cd3a03141885e86c378a082c4bec301af9e7a
quicaoit77 <quicaoit@gmail.com>
2021-11-26 17:36:22 +0700

    update

--- src/templates/homeTemplate/layout/footer/FooterComponent.jsx
+++ src/templates/homeTemplate/layout/footer/FooterComponent.jsx
@@ -71,13 +71,13 @@ export default function FooterComponent() {
         </div>
         <div className="footer__bot">
           <p>
-            2021 © B.Movie | Booking Ticket Website | By{" "}
-            <a
+            2021 © B.Movie | Booking Ticket Website | By Colin_Wilbus
+            {/* <a
               href="https://portfolio-colin-wilbus.vercel.app/"
               target="_blank"
             >
               Colin_Wilbus
-            </a>
+            </a> */}
           </p>
         </div>
       </div>
